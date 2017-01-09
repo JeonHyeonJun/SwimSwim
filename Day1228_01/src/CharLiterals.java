a64d568808954965468770297c3bcec4f5d7e309
student <student@M110215>
2017-01-09 16:40:44 +0900

    한글깨져서 수정

--- Day1228_01/src/CharLiterals.java
+++ Day1228_01/src/CharLiterals.java
@@ -1,10 +1,10 @@
 
 public class CharLiterals {
 	public static void main(String[] args) {
-		char a = '°¡';
+		char a = '?';
 		char b = 'A';
 		char c = '\u0041';
 		boolean d = true;
-		System.out.println("½ÇÇà°æ°ú :" + a + " " + b + " " + c + " " + d);
+		System.out.println("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ :" + a + " " + b + " " + c + " " + d);
 	}
 }
