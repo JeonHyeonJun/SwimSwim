11bac4e8db1c4ec3417c1f34dd030c2116b1040b
student <student@M110215>
2017-01-10 15:43:54 +0900

    의존관계역전

--- Day0110_01/src/v4/RobotTest.java
+++ Day0110_01/src/v4/RobotTest.java
@@ -1,8 +1,8 @@
-package v4;
+package v4;	//리스코프치환원칙을 적용하여 멤버변수의 다형성을 넣을것이다 v5에서 ㅇㅇ
 
 public class RobotTest {
 	public static void main(String[] args) {
-		PunchAttack pa = new PunchAttack();
+		PunchAttack pa = new PunchAttack();			//공격 둘과 이동 둘간의 상관관계가없음
 		MissileAttack ma = new MissileAttack();
 		WalkingMove wm = new WalkingMove();
 		FlyingMove fm = new FlyingMove();
