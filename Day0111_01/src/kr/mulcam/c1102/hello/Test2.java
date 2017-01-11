adae105ae6920c749d2fdf14a070d6390a609373
student <student@M110215>
2017-01-11 15:47:50 +0900

    yyyy-MM-dd형식 날짜를 다른형식으로 출력하기

--- Day0111_01/src/kr/mulcam/c1102/hello/Test2.java
+++ Day0111_01/src/kr/mulcam/c1102/hello/Test2.java
@@ -10,7 +10,7 @@ public class Test2 {
 		
 		String s;
 		System.out.println("yyyy-MM-dd hh:mm:ss 로 입력");
-		s = scan.nextLine();
+		s = scan.nextLine();		//next()는 띄어쓰기 전까지 인식하므로 nextLine()을 써야한다!
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		
