5d19d8e40e9bc0fba44337c702846276917740b8
student <student@M110215>
2017-01-13 09:22:49 +0900

    연산예외처리

--- Day0113_01/src/DivideByZero.java
+++ Day0113_01/src/DivideByZero.java
@@ -3,7 +3,15 @@ public class DivideByZero {
 	public static void main(String[] args) {
 		int x = 1;
 		int y = 0;
-		int result = x/y;
-		System.out.println("이 문장이 출력될꺼같냐");
+
+		try {
+			int result = x / y;
+		} catch (ArithmeticException e) {
+			System.out.println("0으론 나눌 수 없습니다");
+		}
+		System.out.println("프로그램은 끝나지 않아요");
+
+		// int result = x/y;
+		// System.out.println("이 문장이 출력될꺼같냐");
 	}
 }
