c7b9c2064ec1b6a98677569d717ea0fcc0517e2c
student <student@M110215>
2017-01-13 09:23:18 +0900

    정수입력할때 정수가 아닐시 예외처리

---
+++ Day0113_01/src/InputError.java
@@ -0,0 +1,17 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputError {
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int i = 0;
+		System.out.println("정수를 입력하시오");
+		try {
+			i = scan.nextInt();
+			System.out.println("입력받은 정수의 값: " + i);
+		} catch (InputMismatchException e) {
+			System.out.println("정수가 아닙니다");
+		}
+	}
+}
