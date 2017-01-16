dade8b2b31277a39c8962b56299943a3334fde45
student <student@M110215>
2017-01-16 10:59:58 +0900

    Thread와 Runnable

--- Day0116_01/src/Test.java
+++ Day0116_01/src/Test.java
@@ -1,9 +1,19 @@
 
-class Counting extends Thread {
+//class Counting extends Thread {
+//	@Override
+//	public void run() {
+//		// TODO Auto-generated method stub
+//		// super.run();
+//		System.out.println(Thread.currentThread());
+//		for (int i = 0; i < 10; i++)
+//			System.out.println(i);
+//	}
+//}
+
+class Counting implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		// super.run();
 		for (int i = 0; i < 10; i++)
 			System.out.println(i);
 	}
@@ -11,7 +21,15 @@ class Counting extends Thread {
 
 public class Test {
 	public static void main(String[] args) {
-		Thread t = new Counting();
-		t.start();
+		Counting c = new Counting();	//Runnable 구현객체를
+		Thread t1 = new Thread(c);		//Thread클래스의 생성자에 전달
+		t1.start();
+		
+//		Thread t1 = new Counting();
+//		t1.start();					//start()는 새로운 명령처리흐름을 만들어서 run()실행하라고 하고 갈길감
+//		Thread t2 = new Counting();
+//		t2.start();
+//		
+//		t1.run();					//이건그냥 run()함수 실행
 	}
 }
