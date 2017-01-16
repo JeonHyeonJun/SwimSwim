df6699acddc5172460c80ceb8238aaa65481501b
student <student@M110215>
2017-01-16 11:04:32 +0900

    Thread와 Runnable

---
+++ Day0116_01/src/TestThread.java
@@ -0,0 +1,23 @@
+
+class MyRunnable implements Runnable{
+	String myName;
+	public MyRunnable(String name){
+		myName = name;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++){
+			System.out.println(myName + i);
+		}
+	}
+}
+
+public class TestThread{
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new MyRunnable("First Thread"));
+		Thread t2 = new Thread(new MyRunnable("Second Thread"));
+		t1.start();
+		t2.start();
+	}
+}
