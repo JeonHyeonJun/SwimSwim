bed90c52f74492e0b32f64a2f1b91bc0cc6c97e3
student <student@M110215>
2017-01-19 17:20:02 +0900

    전자출결v0.85

--- Project01_1/src/attend/AttendanceSystem.java
+++ Project01_1/src/attend/AttendanceSystem.java
@@ -12,36 +12,15 @@ public class AttendanceSystem {
 	private static Scanner scan = new Scanner(System.in);
 	static List<Student> st = new ArrayList<Student>();
 	static List<Professor> prof = new ArrayList<Professor>();
+
 	public static void main(String[] args) {
 		int num = 0; // 내가 입력할 출석번호
 		int fail = 0; // 출석번호입력 실패횟수
 		boolean run = true; // 프로그램종료
-		final int ATTENDENCE = 15; // 총 수업수
 
-//		// 학생 생성
-//		st.add(new Student("id1", "ps1", "김모범"));
-//		st.add(new Student("id2", "ps2", "김불량"));
-//		st.add(new Student("id3", "ps3", "김평범"));
-//		//교수 생성
-//		prof.add(new Professor("prof01", "123", "김교수"));
-//		prof.add(new Professor("prof02", "456", "이교수"));
-//		prof.add(new Professor("prof03", "789", "박교수"));
-//		// 학생마다 수강과목생성
-//		st.get(0).add(101, "철학", 0, 4);
-//		st.get(0).add(102, "물리학", 0, 4);
-//		st.get(1).add(103, "과학", 0, 1);
-//		st.get(1).add(102, "물리학", 0, 0);
-//		st.get(2).add(104, "철학", 0, 3);
-//		//각 교수 강의 목록
-//		prof.get(0).add(101, "철학", 0, 4);
-//		prof.get(0).add(102, "물리학", 0, 4);
-//		prof.get(1).add(103, "과학", 0, 1);
-//		prof.get(1).add(102, "물리학", 0, 0);
-//		prof.get(2).add(102, "물리학", 0, 4);
-//		prof.get(2).add(103, "과학", 0, 1);
-		// 프로그램시작
-		
 		load();
+
+		// 프로그램시작
 		while (run) {
 			try {
 				System.out.println("-------------------");
@@ -80,7 +59,6 @@ public class AttendanceSystem {
 
 									while (fail < 3 && run) {
 
-
 										// 출석번호입력
 										System.out.println(st.get(i).getSubject().get(k).getNumber());
 										System.out.print("출석번호> ");
@@ -91,21 +69,21 @@ public class AttendanceSystem {
 											System.out.println("출석이 완료되었습니다.");
 											int tmp = st.get(i).getSubject().get(k)
 													.setAttendance(st.get(i).getSubject().get(k).getAttendance() + 1);
-											System.out.println("출석 " + tmp +"회 완료");
+											System.out.println("출석 " + tmp + "회 완료");
 											st.get(i).getSubject().get(k).setNumber(0);
 											run = false;
 											save();
-										// 출석실패
+											// 출석실패
 										} else {
 											fail++;
 											System.out.println("출석번호가 일치하지않습니다. " + fail + "번실패");
 										}
 									} // 출석 while 끝
-									
+
 									fail = 0; // 출석실패횟수 초기화
-									
+
 								} // 강의선택성공끝
-							
+
 							if (subselect == false) {
 								System.out.println("수강중인 강의가 아닙니다.");
 							}
@@ -138,24 +116,22 @@ public class AttendanceSystem {
 							select = scan.nextInt();
 
 							// 출석체크
-							for (int k = 0; k <prof.get(i).getSubject().size(); k++)
+							for (int k = 0; k < prof.get(i).getSubject().size(); k++)
 								if (select == prof.get(i).getSubject().get(k).getSubjectcode()) {
 									subselect = true;
-									
+
 									// 출석번호출력
 									int answer = (int) (Math.random() * 100) + 1;
 									System.out.println(answer);
-									for(int j=0; j<st.size(); j++){
-										for(int q=0; q<st.get(j).getSubject().size(); q++){
-											if(st.get(j).getSubject().get(q).getSubjectcode() == prof.get(i).getSubject().get(k).getSubjectcode())
+									for (int j = 0; j < st.size(); j++) {
+										for (int q = 0; q < st.get(j).getSubject().size(); q++) {
+											if (st.get(j).getSubject().get(q).getSubjectcode() == prof.get(i)
+													.getSubject().get(k).getSubjectcode())
 												st.get(j).getSubject().get(q).setNumber(answer);
 										}
 									}
-										
-//										answer = st.get(i).getSubject().get(k).setNumber((int) (Math.random() * 100) + 1);
-									
 								} // 강의선택성공끝
-							
+
 							if (subselect == false) {
 								System.out.println("수강중인 강의가 아닙니다.");
 							}
@@ -165,7 +141,7 @@ public class AttendanceSystem {
 					// id,ps불일치
 					if (login == false)
 						System.out.println("id 또는 password가 일치하지 않습니다.");
-						
+
 					// 종료
 				} else if (select == 3) {
 					run = false;
@@ -181,31 +157,31 @@ public class AttendanceSystem {
 			}
 		} // 전체while
 	}// main
-	
-	//불러오기
-	public static void load(){
+
+	// 불러오기
+	public static void load() {
 		ObjectInputStream stin = null;
 		ObjectInputStream prin = null;
-		
+
 		try {
 			stin = new ObjectInputStream(new FileInputStream("st.dat"));
 			prin = new ObjectInputStream(new FileInputStream("pr.dat"));
-			
+
 			st = (List<Student>) stin.readObject();
 			prof = (List<Professor>) prin.readObject();
-			 
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			st = new ArrayList<Student>();
 			prof = new ArrayList<Professor>();
-			
+
 			// 학생 생성
 			st.add(new Student("id1", "ps1", "김모범"));
 			st.add(new Student("id2", "ps2", "김불량"));
 			st.add(new Student("id3", "ps3", "김평범"));
-			//교수 생성
+			// 교수 생성
 			prof.add(new Professor("prof01", "123", "김교수"));
 			prof.add(new Professor("prof02", "456", "이교수"));
 			prof.add(new Professor("prof03", "789", "박교수"));
@@ -215,34 +191,33 @@ public class AttendanceSystem {
 			st.get(1).add(103, "과학", 0, 0);
 			st.get(1).add(102, "물리학", 0, 0);
 			st.get(2).add(104, "철학", 0, 0);
-			//각 교수 강의 목록
+			// 각 교수 강의 목록
 			prof.get(0).add(101, "철학", 0, 4);
 			prof.get(0).add(102, "물리학", 0, 4);
 			prof.get(1).add(103, "과학", 0, 1);
 			prof.get(1).add(102, "물리학", 0, 0);
 			prof.get(2).add(102, "물리학", 0, 4);
 			prof.get(2).add(103, "과학", 0, 1);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
-		finally {
+		} finally {
 			try {
-				if(stin != null)
+				if (stin != null)
 					stin.close();
-				if(stin != null)
+				if (stin != null)
 					prin.close();
-			} catch (IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	//저장하기
-	public static void save(){
+
+	// 저장하기
+	public static void save() {
 		ObjectOutputStream stout = null;
 		ObjectOutputStream prout = null;
-		
+
 		try {
 			stout = new ObjectOutputStream(new FileOutputStream("st.dat"));
 			prout = new ObjectOutputStream(new FileOutputStream("pr.dat"));
@@ -253,10 +228,10 @@ public class AttendanceSystem {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			try{
-				if(stout != null)
+			try {
+				if (stout != null)
 					stout.close();
-				if(prout != null)
+				if (prout != null)
 					prout.close();
 			} catch (IOException e) {
 				e.printStackTrace();
