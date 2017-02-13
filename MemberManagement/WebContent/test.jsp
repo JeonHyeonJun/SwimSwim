f4020e98422ee9c803b38b5ff8d98cdb4b19c8eb
student <student@M110227>
2017-02-13 17:53:28 +0900

    회원관리

---
+++ MemberManagement/WebContent/test.jsp
@@ -0,0 +1,21 @@
+<%@page import="javax.naming.InitialContext"%>
+<%@page import="java.sql.Connection"%>
+<%@page import="javax.sql.DataSource"%>
+<%@page import="javax.naming.Context"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		Context ic = new InitialContext();
+		DataSource ds = (DataSource)ic.lookup("java:comp/env/jdbc/member_db");
+		Connection con = ds.getConnection();
+		out.println("디비연결성공");
+	%>
+</body>
+</html>
---
+++ MemberManagement/src/dto/MemberDao.java
@@ -0,0 +1,181 @@
+package dto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDao {
+	private static MemberDao instance;
+
+	public static MemberDao getInstance() {
+		if (instance == null)
+			instance = new MemberDao();
+		return instance;
+	}
+
+	public Connection getConnection() throws Exception {
+		Context ic = new InitialContext();
+		DataSource ds = (DataSource)ic.lookup("java:comp/env/jdbc/member_db");
+		Connection con = ds.getConnection();
+		return con;
+	}
+	
+	//로그인체크
+	public int userCheck(String userid, String pwd) {
+		int result = -1;
+		String sql = "select pwd from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString("pwd") != null && rs.getString("pwd").equals(pwd)) {
+					result = 1;
+				} else {
+					result = 0;
+				}
+			} else {
+				result = -1;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	//회원정보가져오기
+	public Member getMember(String userid) {
+		Member member = null;
+		String sql = "select * from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				member = new Member();
+				member.setName(rs.getString("name"));
+				member.setUserid(rs.getString("userid"));
+				member.setPwd(rs.getString("pwd"));
+				member.setEmail(rs.getString("email"));
+				member.setPhone(rs.getString("phone"));
+				member.setAdmin(rs.getInt("admin"));
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return member;
+	}
+	
+	//아이디 중복체크
+	public int confirmID(String userid){
+		int result = -1;
+		String sql = "select userid from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next())
+				result = 1;
+			else
+				result = -1;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	//회원정보 추가하기
+	public int insertMember(Member member){
+		int result = -1;
+		String sql = "insert into member values(?,?,?,?,?,?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getName());
+			pstmt.setString(2, member.getUserid());
+			pstmt.setString(3, member.getPwd());
+			pstmt.setString(4, member.getEmail());
+			pstmt.setString(5, member.getPhone());
+			pstmt.setInt(6, member.getAdmin());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;		
+	}
+}
